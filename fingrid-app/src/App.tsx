0081d6fe9e80752ba183557cb7dcdf29fac447a7
Joonatan Korpela <joonatan.korpela@aalto.fi>
2024-11-09 15:26:54 +0200

    feat: Make items expandable

--- fingrid-app/src/App.tsx
+++ fingrid-app/src/App.tsx
@@ -23,24 +23,48 @@ function App() {
     defaultData as unknown as FingridData[],
   );
 
+  const [expandedIndex, setExpandedIndex] = useState<number | null>(null);
+
+  const toggleExpand = (index: number) => {
+    setExpandedIndex(expandedIndex === index ? null : index);
+  };
+
+  const importantFields = ['Kehitysehdotus', 'Nostopäiväys', 'Prioriteetti'];
+
   return (
     <div className="p-4">
-      <div className="w-full flex items-center">
+      <div className="w-full flex flex-col items-center gap-3 p-3">
         <img
           className="h-[3rem]"
           src="https://upload.wikimedia.org/wikipedia/fi/c/cb/Fingrid_logo.svg"
         />
+        <h1 className="text-2xl font-bold mb-4">
+          Development Management System
+        </h1>
       </div>
-      <h1 className="text-2xl font-bold mb-4">Development Management System</h1>
       <div className="grid grid-cols-1 gap-4">
         {data.map((entry, index) => (
           <div key={index} className="p-4 border rounded shadow">
-            {Object.entries(entry).map(([key, value]) => (
-              <div key={key} className="mb-2">
-                <span className="font-semibold">{key}: </span>
-                <span>{value}</span>
-              </div>
-            ))}
+            <button
+              className="text-blue-500 underline m-3 float-right"
+              onClick={() => toggleExpand(index)}
+            >
+              {expandedIndex === index ? 'Show Less' : 'Show More'}
+            </button>
+            {Object.entries(entry).map(([key, value]) => {
+              if (
+                expandedIndex !== index &&
+                !importantFields.some((field) => key.includes(field))
+              ) {
+                return null;
+              }
+              return (
+                <div key={key} className="mb-2">
+                  <span className="font-semibold">{key}: </span>
+                  <span>{value}</span>
+                </div>
+              );
+            })}
           </div>
         ))}
       </div>
